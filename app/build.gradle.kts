91ec1ab720f97dba6c81e213b17b37ac667c15c7
20221tiimi0300 <marcellagaurink@gmail.com>
2024-10-11 10:59:29 -0300

    passo 8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation (libs.paging.guava)
+    implementation (libs.paging.runtime)
 }
